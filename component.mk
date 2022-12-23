b0d8a3331b67a8120f8cddf59c93e9658bd51b8a
Fred Dushin <fred@dushin.net>
2022-12-22 20:16:47 -0500

    Fix for AtomVM code reorganization.

--- component.mk
+++ component.mk
@@ -1,4 +1,4 @@
-COMPONENT_ADD_INCLUDEDIRS := nifs/include ../../main
+COMPONENT_ADD_INCLUDEDIRS := nifs/include
 COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
 COMPONENT_SRCDIRS := nifs
 CXXFLAGS += -fno-rtti
